26123f140aca29c7e29c5bcc29a13da5db22573f
Chris Hawkins <chris.hawkins@accenture.com>
2016-01-23 04:49:54 +1100

    Cassandra now runs in fg

--- cassandra/startup.sh
+++ cassandra/startup.sh
@@ -2,8 +2,5 @@
 
 export IP_ADDR=$(hostname -I)
 echo "broadcast_rpc_address: $IP_ADDR" >> /etc/cassandra/cassandra.yaml
-
-service cassandra start
-sleep 10
-tail -f /var/log/cassandra/*
-
+echo "IP Address is: $IP_ADDR"
+cassandra -f
